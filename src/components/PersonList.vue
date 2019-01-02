c1575e0928ba8698befc073a45ef6a6bbfdb3fc4
Johnny <syonip@gmail.com>
2019-01-02 14:58:10 +0200

    added db loading

--- src/components/PersonList.vue
+++ src/components/PersonList.vue
@@ -1,33 +1,66 @@
 <template>
   <div>
-    {{ response }}
+    {{ persondb.length }}
   </div>
 </template>
 
 <script>
 const axios = require('axios');
+import { nSQL } from "nano-sql";
 
 export default {
   name: 'PersonList',
   data() {
     return {
-      response: ''
+      persondb: []
     }
   },
   mounted() {
-    document.addEventListener(
-      typeof cordova !== 'undefined' ? 'deviceready' : 'DOMContentLoaded',
-      () => {
-        axios
-          .get('MOCK_DATA.csv')
-          .then(response => {
-            this.response = response;
-          })
-          .catch(err => {
-            this.response = `ERROR! ${err}`;
+    nSQL().onConnected(() => {
+      axios.get("MOCK_DATA.csv", {}).then(response => {
+        nSQL()
+          .loadCSV("persondb", response.data)
+          .then(() => {
+            nSQL("persondb")
+              .query("select")
+              .exec()
+              .then(rows => {
+                this.persondb = rows;
+              });
           });
+      });
+    });
+
+    document.addEventListener(
+      typeof cordova !== "undefined" ? "deviceready" : "DOMContentLoaded",
+      () => {
+        let model = [
+          { key: "id", type: "int", props: ["pk", "ai"] },
+          { key: "first_name", type: "string" },
+          { key: "last_name", type: "string" },
+          { key: "email", type: "string" },
+          { key: "gender", type: "string" },
+          { key: "ip_address", type: "string" }
+        ];
+        if (window.nSQLite && window.cordova.platformId != "browser") {
+          nSQL("persondb")
+            .model(model)
+            .config({
+              mode: window.nSQLite.getMode() // required
+            })
+            .connect();
+        } else {
+          nSQL("persondb")
+            .model(model)
+            .connect();
+        }
       }
     );
+  },
+  methods: {
+    nSQLonChange: function(data) {
+      this.persondb = data;
+    }
   }
 };
 </script>
