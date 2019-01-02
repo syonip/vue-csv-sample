5951134ab5647915705b9f17be38510a9987d991
Johnny <syonip@gmail.com>
2019-01-02 14:41:20 +0200

    first

---
+++ src/components/PersonList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ response }}
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  name: 'PersonList',
+  data() {
+    return {
+      response: ''
+    }
+  },
+  mounted() {
+    document.addEventListener(
+      typeof cordova !== 'undefined' ? 'deviceready' : 'DOMContentLoaded',
+      () => {
+        axios
+          .get('MOCK_DATA.csv')
+          .then(response => {
+            this.response = response;
+          })
+          .catch(err => {
+            this.response = `ERROR! ${err}`;
+          });
+      }
+    );
+  }
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  baseUrl: '',
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
+  }
+}
